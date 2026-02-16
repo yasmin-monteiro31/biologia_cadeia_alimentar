e9f370f04c1927900dafa901a049f982a55e98f6
yasmin-monteiro31 <yasminmapmonteiro@gmail.com>
2026-02-16 20:52:43 -0300

    Add files via upload
    biologia

---
+++ index.js
@@ -0,0 +1,68 @@
+function mostrarSeta() {
+    document.getElementById("seta").classList.toggle("ativo"); 
+    document.getElementById("seta2").classList.toggle("ativo");
+    document.getElementById("raposa").classList.toggle("ativo");
+    document.getElementById("coelho").classList.toggle("ativo");
+    document.getElementById("seta3").classList.toggle("ativo");
+    document.getElementById("cenoura").classList.toggle("ativo");
+
+  }
+  
+  function mostrarSeta2() {
+    document.getElementById("seta4").classList.toggle("ativo");
+    document.getElementById("cobra").classList.toggle("ativo");
+    document.getElementById("seta5").classList.toggle("ativo");  
+    document.getElementById("sapo").classList.toggle("ativo");
+    document.getElementById("seta6").classList.toggle("ativo");  
+    document.getElementById("grilo").classList.toggle("ativo");
+    document.getElementById("seta7").classList.toggle("ativo");  
+    document.getElementById("grama").classList.toggle("ativo");
+  }
+  function mostrarSeta3() {
+    document.getElementById("seta8").classList.toggle("ativo");
+    document.getElementById("peixe").classList.toggle("ativo");
+    document.getElementById("seta9").classList.toggle("ativo");  
+    document.getElementById("lambari").classList.toggle("ativo");
+    document.getElementById("seta10").classList.toggle("ativo");  
+    document.getElementById("caramujo").classList.toggle("ativo");
+    document.getElementById("seta11").classList.toggle("ativo");  
+    document.getElementById("algas").classList.toggle("ativo");
+  }
+
+  function mostrarSeta4() {
+    document.getElementById("seta12").classList.toggle("ativo");
+    document.getElementById("passaro").classList.toggle("ativo");
+    document.getElementById("seta13").classList.toggle("ativo");  
+    document.getElementById("lagarta").classList.toggle("ativo");
+    document.getElementById("seta14").classList.toggle("ativo");  
+    document.getElementById("folhas").classList.toggle("ativo");
+
+  }
+  
+  function mostrarSeta5() {
+    document.getElementById("seta15").classList.toggle("ativo");
+    document.getElementById("cobra3").classList.toggle("ativo");
+    document.getElementById("seta16").classList.toggle("ativo");  
+    document.getElementById("rato").classList.toggle("ativo");
+    document.getElementById("seta17").classList.toggle("ativo");  
+    document.getElementById("capim").classList.toggle("ativo");
+
+  }
+
+  function mostrarSeta6() {
+    document.getElementById("seta18").classList.toggle("ativo");
+    document.getElementById("vaca").classList.toggle("ativo");
+    document.getElementById("seta19").classList.toggle("ativo");  
+    document.getElementById("capim2").classList.toggle("ativo");
+  }
+
+  function mostrarSeta7() {
+    document.getElementById("seta20").classList.toggle("ativo");
+    document.getElementById("galinha").classList.toggle("ativo");
+    document.getElementById("seta21").classList.toggle("ativo");  
+    document.getElementById("milho").classList.toggle("ativo");
+  }
+  
+  function toggleCuriosidade(card) {
+    card.classList.toggle("ativo");
+}
